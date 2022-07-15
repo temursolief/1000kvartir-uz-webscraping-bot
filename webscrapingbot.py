57253e272bb719428ac7cd4294f11045ad7d9248
Timur Soliev <96443276+timursolief@users.noreply.github.com>
2022-07-15 13:51:28 +0500

    first commit

---
+++ webscrapingbot.py
@@ -0,0 +1,214 @@
+import logging
+from typing import Dict
+
+import requests
+from bs4 import BeautifulSoup
+from telegram import (InputMedia, KeyboardButton, Location, ParseMode,
+                      ReplyKeyboardMarkup, ReplyKeyboardRemove, Update)
+from telegram.ext import (CallbackContext, CommandHandler, ConversationHandler,
+                          Filters, MessageHandler, Updater)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+TELEGRAM_TOKEN = ""
+
+def start_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard = [
+            [KeyboardButton("Sotib olish"), 
+             KeyboardButton("Ijaraga olish")]
+        ],
+        resize_keyboard=True
+    )
+BUY = 0
+RENT = 0
+
+def start(update:Update, context: CallbackContext):
+    update.message.reply_text(text=f"Salom {update.message.from_user.first_name}.\n\nSiz 1000kvartir.uz saytining Telegram botidan foydalanyapsiz.", reply_markup=start_keyboard())
+
+
+def rent_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="1"),
+             KeyboardButton(text="2"),
+             KeyboardButton(text="3"),
+             KeyboardButton(text="4")
+             ],
+            [KeyboardButton(text="5"),
+             KeyboardButton(text="6"),
+             KeyboardButton(text="7"),
+             KeyboardButton(text="8")],
+        ],
+        resize_keyboard=True
+    )
+
+
+def rent(update:Update, context:CallbackContext):
+    update.message.reply_text(
+        text="Ijaraga olmoqchi bo'lsangiz, unda quyidagi sonlardan birini bosing, va men sizga vebsaytning o'sha betidagi natijalarni yuboraman", reply_markup=rent_keyboard())
+    return RENT
+
+
+def buy_results(update: Update, context: CallbackContext):
+    chat_id = update.message.chat.id
+    page_number = update.message.text
+
+    try:
+        url = ""
+        if page_number == "1":
+            url = "https://www.1000kvartir.uz/mr/sale"
+        else:
+            url = f"https://www.1000kvartir.uz/mr/sale?page={page_number}"
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+        page = requests.get(url.strip(), headers=headers, timeout=10)
+        soup = BeautifulSoup(page.text, "html.parser")
+
+        general_descriptions = soup.find_all(
+            "div", attrs={"class": "list-offers list-offers_mod"})
+        for general_desc in general_descriptions:
+            location = general_desc.find(
+                "h4", attrs={"style": "margin-top:0; margin-bottom:10px;"}).text.strip()
+            area = general_desc.find("li", attrs={"title": "Площадь"}).text.strip()
+            floor = general_desc.find(
+                "li", attrs={"title": "Этажность"}).text.strip()
+            roomNum = general_desc.find(
+                "li", attrs={"title": "Кол-во комнат"}).text.strip()
+            price = general_desc.find(
+                "span", attrs={"class": "desc-of1__price-of desc-of1__price-of_mod"}).text.strip()
+            image = general_desc.find(
+                "img", attrs={"class": "carousel-img"})["src"]
+            imag = general_desc.find(
+                "img", attrs={"class": "carousel-img"})
+            link = imag.find_parent("a")["href"]
+
+            image_src = f"https://www.1000kvartir.uz{image}"
+            caption = f"{location}\n\n{area}\t{floor}\t{roomNum}\n\n{price}\n\nFor more: https://www.1000kvartir.uz{link}"
+            context.bot.send_photo(chat_id, image_src, caption)
+    except:
+        context.bot.send_message(
+                chat_id, "Kechirasiz, sotuvga hech nima topilmadi 🤔", reply_markup=start_keyboard())
+    return ConversationHandler.END
+  
+
+def buy_keyboard():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="1"),
+             KeyboardButton(text="2"),
+             KeyboardButton(text="3"),
+             KeyboardButton(text="4")
+             ],
+            [KeyboardButton(text="5"),
+             KeyboardButton(text="6"),
+             KeyboardButton(text="7"),
+             KeyboardButton(text="8")],
+        ],
+        resize_keyboard=True
+    )
+
+
+def buy(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text="Sotib olmoqchi bo'lsangiz, unda quyidagi sonlardan birini bosing, va men sizga vebsaytning o'sha betidagi natijalarni yuboraman", reply_markup=buy_keyboard())
+    return BUY
+
+def rent_results(update: Update, context: CallbackContext):
+    chat_id = update.message.chat.id
+    page_number = update.message.text
+    
+    try:
+        url = ""
+        if page_number == "1":
+            url = "https://www.1000kvartir.uz/mr/rent"
+        else:
+            url = f"https://www.1000kvartir.uz/mr/rent?page={page_number}"
+            
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+        page = requests.get(url.strip(), headers=headers, timeout=10)
+        soup = BeautifulSoup(page.text, "html.parser")
+
+        general_descriptions = soup.find_all("div", attrs={"class": "list-offers list-offers_mod"})
+        for general_desc in general_descriptions:
+            location = general_desc.find(
+                "h4", attrs={"style": "margin-top:0; margin-bottom:10px;"}).text.strip()
+            area = general_desc.find("li", attrs={"title": "Площадь"}).text.strip()
+            floor = general_desc.find(
+                "li", attrs={"title": "Этажность"}).text.strip()
+            roomNum = general_desc.find(
+                "li", attrs={"title": "Кол-во комнат"}).text.strip()
+            price = general_desc.find(
+                "span", attrs={"class": "desc-of1__price-of desc-of1__price-of_mod"}).text.strip()
+            image = general_desc.find(
+                "img", attrs={"class": "carousel-img"})["src"]
+            imag = general_desc.find(
+                "img", attrs={"class": "carousel-img"})
+            link = imag.find_parent("a")["href"]
+
+            image_src = f"https://www.1000kvartir.uz{image}"
+            caption = f"{location}\n\n{area}\t{floor}\t{roomNum}\n\n{price}\n\nFor more: https://www.1000kvartir.uz{link}"
+            context.bot.send_photo(chat_id, image_src, caption)
+        context.bot.send_message(
+            chat_id, "Mana senga olam-olam uuuuuy! 🎶\nEtagingga siqqanicha oooool! 😂")
+    except:
+        context.bot.send_message(
+            chat_id, "Kechirasiz, hech nima topa olmadim 🤔", reply_markup=start_keyboard())
+    return ConversationHandler.END
+
+def main():
+    
+    updater = Updater(TELEGRAM_TOKEN)
+    
+    dispatcher = updater.dispatcher
+    
+    dispatcher.add_handler(CommandHandler("start", start))
+    
+    buy_conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(
+            Filters.text("Sotib olish"), buy)],
+        states = {
+            BUY: [(MessageHandler(Filters.text("1"), buy_results)),
+                  (MessageHandler(Filters.text("2"), buy_results)),
+                  (MessageHandler(Filters.text("3"), buy_results)),
+                  (MessageHandler(Filters.text("4"), buy_results)),
+                  (MessageHandler(Filters.text("5"), buy_results)),
+                  (MessageHandler(Filters.text("6"), buy_results)),
+                  (MessageHandler(Filters.text("7"), buy_results)),
+                  (MessageHandler(Filters.text("8"), buy_results))]
+        },
+        fallbacks=[CommandHandler("start", start)]
+        )
+    rent_conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(
+            Filters.text("Ijaraga olish"), rent)],
+        states={
+            RENT: [(MessageHandler(Filters.text("1"), rent_results)),
+                  (MessageHandler(Filters.text("2"), rent_results)),
+                  (MessageHandler(Filters.text("3"), rent_results)),
+                  (MessageHandler(Filters.text("4"), rent_results)),
+                  (MessageHandler(Filters.text("5"), rent_results)),
+                  (MessageHandler(Filters.text("6"), rent_results)),
+                  (MessageHandler(Filters.text("7"), rent_results)),
+                  (MessageHandler(Filters.text("8"), rent_results))]
+        },
+        fallbacks=[CommandHandler("start", start)]
+        )
+    
+    
+    dispatcher.add_handler(buy_conv_handler)
+    dispatcher.add_handler(rent_conv_handler)
+    
+
+    updater.start_polling()
+    updater.idle()
+    
+if __name__=="__main__":
+    main()
